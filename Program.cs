ad6d6630ff47ba5b3a5be74e36d3d75987945f46
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-07 14:00:35 -0500

    Putting down code for part 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name;
+            int age, currentYear, birthYear;
+            currentYear = DateTime.Now.Year;
+
+            Console.WriteLine("Please write down your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Please write down your age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            birthYear = currentYear - age;
+
+            Console.WriteLine($"Hello {name}! You were born in {birthYear}! Wow!");
         }
     }
 }
