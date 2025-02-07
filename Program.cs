b6c2f3f4b84eeb2da0cfd6f89620d983cc5d9673
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-07 14:19:09 -0500

    Done both Problem 1 and 2

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@
         {
             string name;
             int age, currentYear, birthYear;
+            double distOne,distTwo, distThree, rightTriOne, rightTriTwo, addNumOne, addNumTwo, addNumThree, total;
             currentYear = DateTime.Now.Year;
 
+            //1. Greeting
             Console.WriteLine("Please write down your name:");
             name = Console.ReadLine();
             Console.WriteLine("Please write down your age:");
@@ -15,6 +17,16 @@
             birthYear = currentYear - age;
 
             Console.WriteLine($"Hello {name}! You were born in {birthYear}! Wow!");
+
+            //2. Adder
+            Console.WriteLine("Now type down three numbers, and I will say its total.");
+            addNumOne = Convert.ToDouble(Console.ReadLine());
+            addNumTwo = Convert.ToDouble(Console.ReadLine());
+            addNumThree = Convert.ToDouble(Console.ReadLine());
+            total = addNumOne + addNumTwo + addNumThree;
+            Console.WriteLine($"Your total is {total}");
+
+
         }
     }
 }
