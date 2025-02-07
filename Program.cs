ca0d5ea849b7346e0f671f8000df101928fecb39
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-07 13:49:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_3___Keyboard_Input_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
